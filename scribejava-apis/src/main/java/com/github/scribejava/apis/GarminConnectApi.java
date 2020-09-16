ef875bb273f8590087bade2927e972ed49125c39
Bryce Pavey <bryce@dreamwalk.com.au>
2020-09-16 12:28:04 +1000

    attempt to fix compile

--- scribejava-apis/src/main/java/com/github/scribejava/apis/GarminConnectApi.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/GarminConnectApi.java
@@ -4,12 +4,13 @@ import com.github.scribejava.core.builder.api.DefaultApi10a;
 import com.github.scribejava.core.model.OAuth1RequestToken;
 
 public class GarminConnectApi extends DefaultApi10a {
+
     private static final String AUTHORIZE_URL = "https://connect.garmin.com/oauthConfirm";
     private static final String REQUEST_TOKEN_RESOURCE = "https://connectapi.garmin.com/oauth-service/oauth/request_token";
     private static final String ACCESS_TOKEN_RESOURCE = "https://connectapi.garmin.com/oauth-service/oauth/access_token";
 
 
-    private GarminConnectApi() {
+    protected GarminConnectApi() {
     }
 
     private static class InstanceHolder {
@@ -28,11 +29,6 @@ public class GarminConnectApi extends DefaultApi10a {
     @Override
     public String getAccessTokenEndpoint() {
         return ACCESS_TOKEN_RESOURCE;
-    }
-
-    @Override
-    protected String getAuthorizationBaseUrl() {
-        return AUTHORIZE_URL;
     }
 
     @Override
