871abfc33ec34d6a9dd513e4af29ea116d594fbc
Bryce Pavey <bryce@dreamwalk.com.au>
2020-09-16 13:42:29 +1000

    fix authorize url

--- scribejava-apis/src/main/java/com/github/scribejava/apis/GarminConnectApi.java
+++ scribejava-apis/src/main/java/com/github/scribejava/apis/GarminConnectApi.java
@@ -5,7 +5,7 @@ import com.github.scribejava.core.model.OAuth1RequestToken;
 
 public class GarminConnectApi extends DefaultApi10a {
 
-    private static final String AUTHORIZE_URL = "https://connect.garmin.com/oauthConfirm";
+    private static final String AUTHORIZE_URL = "https://connect.garmin.com/oauthConfirm?oauth_token=%s";
     private static final String REQUEST_TOKEN_RESOURCE = "https://connectapi.garmin.com/oauth-service/oauth/request_token";
     private static final String ACCESS_TOKEN_RESOURCE = "https://connectapi.garmin.com/oauth-service/oauth/access_token";
 
